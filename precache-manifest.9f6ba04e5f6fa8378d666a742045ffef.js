d32ffa6fd2232d740838d642065dec0f48960a5e
Zsolt Balvanyos <Zsolt.Balvanyos@ktech.com>
2019-11-01 13:06:58 +0000

    Updates

---
+++ precache-manifest.9f6ba04e5f6fa8378d666a742045ffef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343f72e7c1a1c9dd671559acc54cb9d8",
+    "url": "/brexit/index.html"
+  },
+  {
+    "revision": "83040e5564d40a1edd4e",
+    "url": "/brexit/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a3d8e2ff1482ed5631eb",
+    "url": "/brexit/static/css/main.71475288.chunk.css"
+  },
+  {
+    "revision": "83040e5564d40a1edd4e",
+    "url": "/brexit/static/js/2.8934452c.chunk.js"
+  },
+  {
+    "revision": "a3d8e2ff1482ed5631eb",
+    "url": "/brexit/static/js/main.c23adf0f.chunk.js"
+  },
+  {
+    "revision": "6fa362cdb54224524127",
+    "url": "/brexit/static/js/runtime-main.7bca68eb.js"
+  }
+]);
